91bac7659c4b215fc0d7c07f620c7da96309e2c0
AustinxEwell <austin.ewell86@gmail.com>
2021-11-13 08:21:36 -0700

    started fetch for current day info, realized moment is no longer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,37 @@ searchEntry.keypress(function(event) {
     }
 })
 
+$('#searchHistoryContainer').on('click', "button", function(event) {
+    var cityName = event.target.innerText;
+    geocode(cityName)
+})
 
+var geocode = function(cityName) {
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=cccf269a77ddb6c94abd87b983498833";
+
+    fetch(apiUrl).then(function(res) {
+        if (res.ok) {
+            res.json().then(function(res) {
+                console.log(res);
+
+                //city name and display
+                var cityName = res[0].name;
+                $('#city').text(cityName);
+
+                //save to local storage
+                saveSearch(cityName);
+
+                //grab lat/lon and send to fetcher function
+                // var lat = res[0].lat
+                // var lon = res[0].lon
+                // fetcher(lat, lon);
+
+                //load local storage
+                loadHistory()
+            })
+        }
+    })
+}
 
 
 
